74a0b9dc03294a8f1050ddaad89d29affc128a59
yashmahto <yashmahto244@gmail.com>
2025-04-21 21:05:05 +0530

    authentication using firebase

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,12 @@
 import{ ReactNode } from 'react'
+import { redirect } from 'next/navigation';
+import { isAuthenticated } from '@/lib/actions/auth.action';
 
-const Authlayout = ({children} : {children : ReactNode}) => {
+const Authlayout = async ({children} : {children : ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+  
+  if(isUserAuthenticated) redirect('/');
+
   return (
     <div className='auth-layout'>{children}</div>
   )
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import  { ReactNode } from 'react'
 import Image from 'next/image';
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-const Rootlayout = ({children} : {children : ReactNode}) => {
+const Rootlayout = async ({children} : {children : ReactNode}) => {
+const isUserAuthenticated = await isAuthenticated();
+
+if(!isUserAuthenticated) redirect('/sign-in');
+
   return (
     <div className='root-layout'>
       <nav>
